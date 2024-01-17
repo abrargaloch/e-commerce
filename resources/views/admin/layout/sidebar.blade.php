d281c9052ad9c4b99046ea8f2a99c28b2387ecaf
abrargaloch <abrargaloch@gmail.com>
2024-01-17 21:43:26 +0500

    admin update password, admin update details added

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -44,7 +44,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="./index.html" class="nav-link">
+                <a href="{{ route('admin.update-password') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Update Password</p>
                 </a>
